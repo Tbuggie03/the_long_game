fbbd32a20d734acd67cc791998717709165b2212
Tommy Seternus <thomassseternus@gmail.com>
2024-11-07 08:21:10 -0500

    Initial commit

---
+++ the-long-game-HW/the-long-game-HW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace the_long_game_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
